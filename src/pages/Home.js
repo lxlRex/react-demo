1e5dcbdbb7ba71d26fbf7de1ed581c2e19024f65
lixiaolong@jiangduoduo.com <lixiaolong@jiangduoduo.com>
2019-02-21 19:56:56 +0800

    upgrade

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react'
 import './home.css'
 import './home.scss'
-import Button from '@/public/components/button'
+import Button from '@public/components/button'
 // import { Button } from 'element-react'
 
 export default class Home extends Component {
+  constructor () {
+    super()
+    this.state = {
+      title: 'hello world'
+    }
+  }
+
   componentWillMount () {
   }
 
   buttonClickHandler () {
-    alert(123)
+    console.log(this.input.value)
+  }
+
+  inputChangeHandler (term, e) {
+    let obj = {}
+    obj[term] = e.target.value
+    this.setState(obj)
   }
 
   render () {
     return (
       <div className="home">
-        qweqeqw
+        {this.state.title}
         <div className="home__text">
           this is home~123
         </div>
-        <Button disabled onClick={this.buttonClickHandler.bind(this)}>啊阿萨德</Button>
+        <input ref={input => { this.input = input }} type="text" value={this.state.title} onChange={e => this.inputChangeHandler('title', e)}/>
+        <Button onClick={this.buttonClickHandler.bind(this)}>啊阿萨德</Button>
       </div>
     )
+  }
+
+  componentDidMount () {
+    console.log(123)
+  }
+
+  componentWillUpdate () {
+  }
+
+  componentDidUpdate () {
   }
 }

f9f8301fa201600bd6ab503c7734bb561d126f21
lixiaolong@jiangduoduo.com <lixiaolong@jiangduoduo.com>
2019-04-01 18:16:33 +0800

    upgrade

--- src/router/utils/router-container.tsx
+++ src/router/utils/router-container.tsx
@@ -1,18 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { renderRoutes } from 'react-router-config'
 
-interface IProps {
-  route: any
-}
+// interface IProps {
+//   route: any
+// }
+//
+// export default class extends Component<IProps> {
+//   render () {
+//     const { route: {routes} } = this.props
+//     console.log(routes)
+//     return (
+//       <>
+//         {renderRoutes(routes)}
+//       </>
+//     )
+//   }
+// }
 
-export default class extends Component<IProps> {
-  render () {
-    const { route: {routes} } = this.props
-    console.log(routes)
-    return (
-      <>
-        {renderRoutes(routes)}
-      </>
-    )
-  }
-}
+export default ({ routes }: { routes: any }) => <>{renderRoutes(routes)}</>
